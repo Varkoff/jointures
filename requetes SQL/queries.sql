ade3adeb6f63aae3c667b8d6495e294c6c532b24
Varkoff <virgileboris96@hotmail.fr>
2020-04-19 17:57:32 +0200

    Ajout de la quête GROUPE

--- requetes SQL/queries.sql
+++ requetes SQL/queries.sql
@@ -1,46 +1,32 @@
---1
+SELECT
+  t.name,
+  COUNT(*) AS nb_student
+FROM
+  team t
+  JOIN player p ON p.team_id = t.id
+GROUP BY
+  t.name
+ORDER BY
+  nb_student DESC;
 
 SELECT
-  w.firstname,
-  w.lastname,
-  p.role,
   t.name
+FROM
+  team t
+  JOIN player p ON p.team_id = t.id
+GROUP BY
+  t.name
+  HAVING COUNT(*) > 14
+ORDER BY
+  t.name ASC;
+
+SELECT
+  CONCAT(firstname, ' ', lastname) AS fullname,
+  DATE_FORMAT(p.enrollment_date, '%W %M %Y') AS enrollment_day
 FROM
   player p
-  LEFT JOIN wizard w ON w.id = p.wizard_id
-  LEFT JOIN team t ON t.id = p.team_id
-ORDER BY
-  t.name ASC,
-  p.role,
-  w.lastname,
-  w.firstname;
-
---2
-SELECT
-  w.firstname,
-  w.lastname
-FROM
-  wizard w
-  LEFT JOIN player p ON p.wizard_id = w.id
+  JOIN wizard w ON p.wizard_id = w.id
 WHERE
-  p.role LIKE 'seeker'
+  DATE_FORMAT(p.enrollment_date, '%W') LIKE 'Monday'
 ORDER BY
-  w.firstname ASC,
-  w.lastname ASC;
-
---3
-
-SELECT
-  w.firstname,
-  w.lastname,
-  p.role,
-  t.name
-FROM
-  wizard w
-  LEFT JOIN player p ON p.wizard_id = w.id
-  LEFT JOIN team t ON p.team_id = t.id
-WHERE
-  t.name IS NULL
-ORDER BY
-  w.firstname ASC,
-  w.lastname ASC;
+  DATE(p.enrollment_date);
