ca42154504a16ad46d7ebae8aa70a00985d8046c
Varkoff <virgileboris96@hotmail.fr>
2020-04-19 20:18:56 +0200

    modif GROUP

--- requetes SQL/queries.sql
+++ requetes SQL/queries.sql
@@ -16,17 +16,20 @@ FROM
   JOIN player p ON p.team_id = t.id
 GROUP BY
   t.name
-  HAVING COUNT(*) > 14
+  HAVING COUNT(*) >= 14
 ORDER BY
   t.name ASC;
 
 SELECT
   CONCAT(firstname, ' ', lastname) AS fullname,
-  DATE_FORMAT(p.enrollment_date, '%W %M %Y') AS enrollment_day
+  DATE_FORMAT(p.enrollment_date, '%W %M %Y') AS enrollment_day,
+  t.name
 FROM
   player p
   JOIN wizard w ON p.wizard_id = w.id
+  JOIN team t on p.team_id = t.id
 WHERE
   DATE_FORMAT(p.enrollment_date, '%W') LIKE 'Monday'
+  AND t.name LIKE 'Gryffindor'
 ORDER BY
   DATE(p.enrollment_date);
